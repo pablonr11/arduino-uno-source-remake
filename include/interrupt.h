b163c1510c5063d572064b8147dd98f2bfd107a8
Pablo <pablonr11@gmail.com>
2026-01-14 17:56:26 +0100

    Basic implementation for interrupts

---
+++ include/interrupt.h
@@ -0,0 +1,50 @@
+#ifndef _INTERRUPT_H
+#define _INTERRUPT_H
+
+// Enable interrupts setting the global interrupt mask.
+#define SEI() __asm__("sei")
+// Disable interrupts setting the global interrupt mask.
+#define CLI() __asm__("cli")
+
+// This is basically the same as the original source
+// - signal indicates it is an interrupt routine so the compiler includes the RETI instruction at the end
+// - used indicates the compiler to not remove the function even if there's direct reference to it
+#define ISR(ivector)                                                      \
+    void ivector(void) __attribute__((signal, used, externally_visible)); \
+    void ivector(void)
+
+#define VECTOR(vector_number) __vector_##vector_number
+
+// Vector names are defined in crtm328p.o file.
+// This file is automatically added to the compilation by the linker.
+// If we don't define an ISR for a vector it will point to __bad_interrupt
+// from the crt file.
+
+// Interrupt vectors. Those are the parameters expected by the ISR macro
+#define INT0_vect VECTOR(1)
+#define INT1_vect VECTOR(2)
+#define PCINT0_vect VECTOR(3)
+#define PCINT1_vect VECTOR(4)
+#define PCINT2_vect VECTOR(5)
+#define WDT_vect VECTOR(6)
+#define TIMER2_COMPA_vect VECTOR(7)
+#define TIMER2_COMPB_vect VECTOR(8)
+#define TIMER2_OVF_vect VECTOR(9)
+#define TIMER1_CAPT_vect VECTOR(10)
+#define TIMER1_COMPA_vect VECTOR(11)
+#define TIMER1_COMPB_vect VECTOR(12)
+#define TIMER1_OVF_vect VECTOR(13)
+#define TIMER0_COMPA_vect VECTOR(14)
+#define TIMER0_COMPB_vect VECTOR(15)
+#define TIMER0_OVF_vect VECTOR(16)
+#define SPI_STC_vect VECTOR(17)
+#define USART_RX_vect VECTOR(18)
+#define USART_UDRE_vect VECTOR(19)
+#define USART_TX_vect VECTOR(20)
+#define ADC_vect VECTOR(21)
+#define EE_READY_vect VECTOR(22)
+#define ANALOG_COMP_vect VECTOR(23)
+#define TWI_vect VECTOR(24)
+#define SPM_READY_vect VECTOR(25)
+
+#endif
