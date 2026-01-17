226d2723f8f9f469e7a77a173fd015889824e08c
Pablo <pablonr11@gmail.com>
2026-01-17 13:05:29 +0100

    Added comment to specify the max value accepted by delayMicroseconds

--- include/timing.h
+++ include/timing.h
@@ -30,7 +30,7 @@ uint32_t micros(void);
 /**
  * @brief Delays for the given amount of microseconds
  * @param microseconds The amount of microseconds to delay.
- * For delays higher than 65535 use delay()
+ * For delays higher than 16383 microseconds use delay()
  */
 void delayMicroseconds(uint16_t microseconds);
 
