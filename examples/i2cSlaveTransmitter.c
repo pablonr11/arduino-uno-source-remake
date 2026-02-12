b145cd17e0230d5e8c91f43d20c53beefb4df3bd
Pablo <pablonr11@gmail.com>
2026-02-12 19:36:30 +0100

    I2C Transmitter test example

--- examples/i2cSlaveTransmitter.c
+++ examples/i2cSlaveTransmitter.c
@@ -5,6 +5,23 @@
 
 #include <stdint.h>
 
+// In this example we are sending through i2c the leds
+// that the master should power on.
+// In the leds array we have pins 5, 6 and 7. Also
+// 0xFE is the code that indicates all pins should
+// be powered, and 0xFF that indicates that all pins
+// should be turned off.
+// This example doesn't make any sense for a real
+// case, but is an example that sends data over I2C
+// and has a visual output in the master side.
+// The master has the core implementation. What I did
+// is connecting a button to pin 3 of the master
+// as INPUT_PULLUP. And 3 leds on pins 5, 6 and 7.
+// When the button is pressed is requests through I2C
+// the next pin, and this (the slave) is responsible of
+// handling which is the next one, and sends it to the
+// master.
+
 #define I2C_SLAVE_ADDR 0x55 // Own i2c address
 
 #define I2C_RX_BUFFER_SIZE (I2C_BUFFER_SIZE + 1)
